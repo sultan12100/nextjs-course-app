d38586ebf82d90906dc8ac2a57d2b1ee9401b5a1
sultan12100 <sultan12100@hotmail.com>
2021-02-16 07:51:51 +0300

    update index

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+import dynamic from 'next/dynamic'
+// import Link from 'next/link'
+
+const SponsoreAd = dynamic(() => import('../src/components/SponsoreAd'), {
+  ssr: false,
+  loading: () => <p>...</p>,
+})
+
+export default ({ content }) => (
+  <div sx={{ height: `calc(100vh - 60px)` }}>
+    <div
+      sx={{
+        variant: 'containers.page',
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
+      <SponsoreAd />
+    </div>
+  </div>
+)
+
+export async function getStaticProps({ params }) {
+  // get data from CMS
+  return {
+    props: {
+      content: {
+        title: 'This is my really nice app.',
+      },
+    },
+  }
+}
