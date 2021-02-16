db474d58c5c5c8b65b9a92a6c7baad051e61c093
sultan12100 <sultan12100@hotmail.com>
2021-02-16 08:25:38 +0300

    update

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 // module.exports = withCSS({})
 
 // old way for versions lower than 9.4
-// const nextEnv = require('next-env')
-// const dotenvLoad = require('dotenv-load')
-// dotenvLoad()
-// const withEnv = nextEnv()
-// module.exports = withEnv()
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+dotenvLoad()
+const withEnv = nextEnv()
+module.exports = withEnv()
 
 // you can export an object
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
